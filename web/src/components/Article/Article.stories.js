19b534487cacd540f2cd512b0af0002d89c9e42e
Sunfee <sdk6@njit.edu>
2023-09-24 16:56:07 -0400

    break in CH7

--- web/src/components/Article/Article.stories.js
+++ web/src/components/Article/Article.stories.js
@@ -1,5 +1,5 @@
 import Article from './Article'
-
+/*
 export const generated = () => {
   return (
     <Article
@@ -13,5 +13,18 @@ export const generated = () => {
     />
   )
 }
+*/
+const ARTICLE = {
+  id: 1,
+  title: 'First Post',
+  body: `Neutra tacos hot chicken prism raw denim, put a bird on it enamel pin post-ironic vape cred DIY. Street art next level umami squid. Hammock hexagon glossier 8-bit banjo. Neutra la croix mixtape echo park four loko semiotics kitsch forage chambray. Semiotics salvia selfies jianbing hella shaman. Letterpress helvetica vaporware cronut, shaman butcher YOLO poke fixie hoodie gentrify woke heirloom.`,
+}
 
+export const full = () => {
+  return <Article article={ARTICLE} />
+}
+
+export const summary = () => {
+  return <Article article={ARTICLE} summary={true} />
+}
 export default { title: 'Components/Article' }
